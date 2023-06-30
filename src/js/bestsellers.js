f02abbb78289d35b928c90a278acae22cbca57b1
DarriaGl <gladchenkodasha13@gmail.com>
2023-06-30 14:54:06 +0300

    Update bestsellers.js

--- src/js/bestsellers.js
+++ src/js/bestsellers.js
@@ -3,7 +3,7 @@ const bookApi = new BookAPI();
 const containerContent = document.querySelector(
   '.content-rendering-container-js'
 );
-
+const toTopBtn = document.querySelector('.to-top');
 bookApi.getSelectedCategoryBooks().then(data => renderBooks(data));
 
 function renderBooks(books) {
@@ -39,3 +39,31 @@ function createCategoryElement(category) {
   `;
   return categoryElement;
 }
+function onScrollToTopBtn() {
+  const offsetScroll = 200;
+  const pageOffset = window.pageYOffset;
+
+  pageOffset > offsetScroll
+    ? refs.toTopBtn.classList.remove('is-hidden')
+    : refs.toTopBtn.classList.add('is-hidden');
+}
+
+function onTopScroll() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'instant',
+  });
+}
+
+function smoothScrollGallery() {
+  const { height } =
+    refs.galleryContainer.firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: height * 2,
+    behavior: 'instant',
+  });
+}
+toTopBtn.addEventListener('click', onTopScroll);
+galleryContainer.addEventListener('click', smoothScrollGallery);
+window.addEventListener('scroll', onScrollToTopBtn);
