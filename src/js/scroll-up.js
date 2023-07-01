2f64a1414fb0f71a1e6ee475b2dc16f0db781572
Rammbest <igor-kutsenko@ukr.net>
2023-07-01 22:14:53 +0300

    hide scroll on start page

--- src/js/scroll-up.js
+++ src/js/scroll-up.js
@@ -1,13 +1,21 @@
- const toTopBtn = document.querySelector('.back-to-top');
- const btnUpWrapper = document.querySelector('.to-top-target');
+const toTopBtn = document.querySelector('.back-to-top');
+const btnUpWrapper = document.querySelector('.to-top-target');
 
-export default function scrollFunction() {
+function scrollFunction() {
   if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
     btnUpWrapper.style.display = 'flex';
   } else {
     btnUpWrapper.style.display = 'none';
   }
 }
+
+window.addEventListener('scroll', () => {
+  scrollFunction();
+});
+
 toTopBtn.addEventListener('click', () => {
   window.scrollTo({ top: 0, behavior: 'smooth' });
 });
+
+// Спочатку приховуємо кнопку
+btnUpWrapper.style.display = 'none';
