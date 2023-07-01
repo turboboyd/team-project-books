d89592ba6ede07b984e7d24017d217b941c5da0b
liulka_s <asl.warmhaus@gmail.com>
2023-07-01 15:13:08 +0200

    deleted modal.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 // import './js/categories';
 import './js/bestsellers';
-import './js/filter'
+import './js/filter';
 import './js/categories';
 
 import { showLoader, hideLoader } from './js/loader';
 
 import './js/support/support';
-import './js/modal';
