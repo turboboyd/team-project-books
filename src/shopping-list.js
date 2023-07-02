a54e54209c58f8b9d3bdb35aea93e72f0b43807b
Denis Daniv <113061164+turboboyd@users.noreply.github.com>
2023-07-02 21:33:13 +0200

    Fix shop

--- src/shopping-list.js
+++ src/shopping-list.js
@@ -1,6 +1,8 @@
 
 import './js/support/support';
+import './js/theme-dark-light';
 import './js/shopping-list/footer-modal-shoplist';
 import './js/scroll-up';
 import './js/header';
 import './js/theme-dark-light';
+
