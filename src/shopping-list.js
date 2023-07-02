3d1db72a76fb0de661509f1c6284771296b8e9b0
Rammbest <igor-kutsenko@ukr.net>
2023-07-02 23:03:23 +0300

    work footer modal and support on shopping list

--- src/shopping-list.js
+++ src/shopping-list.js
@@ -1,5 +1,6 @@
-import './js/header';
-import './js/support/support';
-import './js/theme-dark-light';
+// import './js/header';
+// import './js/theme-dark-light';
 import './js/shopping-list/footer-modal-shoplist';
+import './js/support/support';
+
 import './js/scroll-up';
