576ed660f0620c4c42430c40f3a531fcd7861e7a
Denis Daniv <113061164+turboboyd@users.noreply.github.com>
2023-07-03 14:05:02 +0200

    Fix shopin list

--- src/shopping-list.js
+++ src/shopping-list.js
@@ -1,7 +1,8 @@
+import './js/firebase';
 import './js/theme-dark-light';
 import './js/support/support';
 import './js/shopping-list/pagination';
 import './js/shopping-list/footer-modal-shoplist';
-import './js/scroll-up';
 import './js/header';
-
+import './js/modal-auth-out';
+import './js/modal-auth';
