e15d1bdd8cb1bf566720dde8cc9a6fb550872f2a
liulka_s <asl.warmhaus@gmail.com>
2023-07-04 01:32:12 +0200

    Update remove-books.js

--- src/js/shopping-list/remove-books.js
+++ src/js/shopping-list/remove-books.js
@@ -12,20 +12,14 @@ import {
   renderBtnList,
 } from './pagination-btn';
 
-// import {
-//   removeFromShoppingList,
-//   getShoppingList,
-//   saveShoppingList,
-// } from '../pop-up-click-by-book';
-
 export default function addEventListenerToTrash() {
   const trashEl = document.querySelectorAll('.shopping-trash');
   trashEl.forEach(el =>
-    el.addEventListener('click', OnClickRemoveBookFromList)
+    el.addEventListener('click', OnClickRemoveBookFromListandStorage)
   );
 }
 
-function OnClickRemoveBookFromList(e) {
+function OnClickRemoveBookFromListandStorage(e) {
   const target = e.target.closest('.shopping-trash');
   const id = target.dataset.id;
   const countPage = btnListPaginationEl.childElementCount;
@@ -40,17 +34,14 @@ function OnClickRemoveBookFromList(e) {
     Number(countPage) === Number(currentPage) &&
     booksCount % countPerPage === 0
   ) {
-    console.log('111');
     setCurrentPage(currentPage - 1);
     renderBtnList(data);
     renderShoppingList(data, currentPage);
   } else {
     if (booksCount === 0) {
-      console.log('222');
       listIsEmpty();
       localStorage.removeItem(shoppingListKey);
     } else {
-      console.log('333');
       renderBtnList(data);
       renderShoppingList(data, currentPage);
     }
@@ -58,19 +49,10 @@ function OnClickRemoveBookFromList(e) {
 }
 
 function removeFromShoppingList(bookId) {
-  const shoppingList = getShoppingList();
+  const shoppingList = parseStorage(shoppingListKey);
   const index = shoppingList.findIndex(book => book._id === bookId);
   if (index !== -1) {
     shoppingList.splice(index, 1);
-    saveShoppingList(shoppingList);
+    localStorage.setItem(shoppingListKey, JSON.stringify(shoppingList));
   }
-}
-
-function getShoppingList() {
-  const shoppingList = JSON.parse(localStorage.getItem(shoppingListKey)) || [];
-  return shoppingList;
-}
-
-function saveShoppingList(shoppingList) {
-  localStorage.setItem(shoppingListKey, JSON.stringify(shoppingList));
 }
