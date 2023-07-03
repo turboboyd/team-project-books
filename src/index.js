7d13ae8ae6513e21d7bc9fa32a028af5e0f49beb
lyashodm <lyashokdima1@gmail.com>
2023-07-03 16:30:06 +0300

    code sharing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 // Authorisation
 import './js/firebase';
-// 
+//
 
 import './js/header';
 import './js/launch';
 import './js/bestsellers';
 import './js/filter';
+import './js/see-more-btn';
 import './js/categories';
 import './js/theme-dark-light';
 import './js/support/support';
@@ -18,5 +19,3 @@ import './js/modal-auth-out';
 import './js/modalcardbook';
 import './js/modal-auth';
 import { showLoader, hideLoader } from './js/loader';
-
-
