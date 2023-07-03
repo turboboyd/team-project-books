0fc656c4dd6be5033c1bbd2f5c15f7e396bbdc44
Rammbest <igor-kutsenko@ukr.net>
2023-07-03 10:40:00 +0300

    add theme dark to shopping list

--- src/shopping-list.js
+++ src/shopping-list.js
@@ -1,7 +1,7 @@
-
+import './js/theme-dark-light';
 import './js/support/support';
 import './js/shopping-list/pagination';
 import './js/shopping-list/footer-modal-shoplist';
 import './js/scroll-up';
 import './js/header';
-import './js/theme-dark-light';
+
