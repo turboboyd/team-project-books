a84c291e4509acd69ef60b7e1594adb85787ca9d
liulka_s <asl.warmhaus@gmail.com>
2023-07-03 14:33:41 +0200

    pagination

--- src/shopping-list.js
+++ src/shopping-list.js
@@ -4,4 +4,4 @@ import './js/shopping-list/pagination';
 import './js/shopping-list/footer-modal-shoplist';
 import './js/scroll-up';
 import './js/header';
-
+// import './js/shopping-list/check-storage';
