71fde4f967ee39f9b4ea6b12512c6f73085512d7
Denis Daniv <113061164+turboboyd@users.noreply.github.com>
2023-07-03 22:16:15 +0200

    Fixed

--- src/index.js
+++ src/index.js
@@ -10,11 +10,10 @@ import './js/see-more-btn';
 import './js/theme-dark-light';
 import './js/support/support';
 
-// import './js/modalcardbook';
+
 import './js/footer-modal-home';
 import './js/pop-up-click-by-book';
 import './js/scroll-up';
 import './js/modal-auth-out';
-import './js/modalcardbook';
 import './js/modal-auth';
 import { showLoader, hideLoader } from './js/loader';
