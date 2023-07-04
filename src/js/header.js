2e307adaba239e5af78b671ae5b0108942f73749
Denis Daniv <113061164+turboboyd@users.noreply.github.com>
2023-07-04 18:34:39 +0200

    Fix

--- src/js/header.js
+++ src/js/header.js
@@ -104,7 +104,8 @@ window.addEventListener('resize', handleResize);
 // const menuBtnStart = document.querySelector('.menu-btn-start-tab');
 const headerButtonsEl = document.querySelector('.header-buttons')
 const headerBtnUserEl = document.querySelector('.header-btn-user-wrap')
-const headerNavLinkEl = document.querySelector('.header-nav')
+const headerNavLinkEl = document.querySelector('.header-list-nav');
+
 // const headerBtnUserMenu = document.querySelector('.header-btn-user-menu');
 
 // export function removeHeaderHidden() {
@@ -124,10 +125,10 @@ export function renderBtnSignupTabDesc() {
 }
 
 // ========== yes registration user ====================
-function renderNavLinkPageTabDesc() {
-  const markupNavLinkPageTabDesc = `<ul class="header-list-nav"><li><a class="header-link-nav current" href="./index.html">HOME</a></li><li><a class="header-link-nav" href="./shopping-list.html">SHOPPING LIST<svg class="header-link-icon" width="20" height="20"><use href="./images/svg-sprite-den.svg#bag"></use></svg></a></li></ul>`;
-  headerNavLinkEl.insertAdjacentHTML('beforeend', markupNavLinkPageTabDesc)
-}
+// function renderNavLinkPageTabDesc() {
+//   const markupNavLinkPageTabDesc = `<ul class="header-list-nav"><li><a class="header-link-nav current" href="./index.html">HOME</a></li><li><a class="header-link-nav" href="./shopping-list.html">SHOPPING LIST<svg class="header-link-icon" width="20" height="20"><use href="./images/svg-sprite-den.svg#bag"></use></svg></a></li></ul>`;
+//   headerNavLinkEl.insertAdjacentHTML('beforeend', markupNavLinkPageTabDesc)
+// }
 
 function renderBtnUserProfTabDesc() {
   const markupUserProfTabDesc = `<button class="header-btn-user" type="button"><div class="btn-container"><div class="header-btn-user-foto"><svg class="header-btn-user-icon" width="19" height="19"><use href="./images/svg-sprite-den.svg#user-default-icon"></use></svg></div><p class="user-name-tablet-desktop">welcome</p><svg class="header-btn-down-icon" width="23" height="26"><use href="./images/svg-sprite-den.svg#user-arrow-down-icon"></use></svg></div></button><div class="header-btn-user-menu is-hidden"><button class="header-menu-btn-exit" type="button">Log out<svg class="menu-btn-start-icon" width="20" height="20"><use href="./images/svg-sprite-den.svg#arrow-right-icon"></use></svg></button></div>`;
@@ -151,13 +152,23 @@ function onOpenMenuUser() {
 
 // ========== render after checking the user in firebase ================  
 export function renderHeaderTabDescLogin(displayName) {
-  renderNavLinkPageTabDesc();
+  // renderNavLinkPageTabDesc();
   // renderBtnLogoutTabDesc();
+  onNavMenu();
+  console.log('делаем рендер');
   renderBtnUserProfTabDesc();
   setUserNameTabDesc(displayName);
+
   console.log('renderHeaderTabDescLogin');
 };
 
 export function renderHeaderTabDescLogout() {
   renderBtnSignupTabDesc();
+}
+
+function onNavMenu() {
+  headerNavLinkEl.classList.remove('is-hidden');
+}
+export function ofNavMenu() {
+  headerNavLinkEl.classList.add('is-hidden');
 }
