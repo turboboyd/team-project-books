e6d8094de4c9957b710a1b4edca28e8e55ec4ffe
Maksym1988 <splien1988@gmail.com>
2023-07-04 20:29:40 +0100

    chenge JS and CSS
    Додана анімація меню

--- src/js/header.js
+++ src/js/header.js
@@ -1,19 +1,19 @@
 import { modalElem } from './modal-auth';
 import { backdropOutEl } from './modal-auth-out';
-import { userVerification, userVerificationTabDesk } from './firebase'
+import { userVerification, userVerificationTabDesk } from './firebase';
 
 const burgerButton = document.querySelector('.burger');
 const closeButton = document.querySelector('.header-btn-close');
 const menuStart = document.querySelector('.mob-menu-start');
 const bodyElement = document.querySelector('body');
 
-  const htmlUserNotLoginModal =
-    '<button data-modal-auth-open class="menu-btn-start" type="button">Sign up<svg class="menu-btn-start-icon" width="20" height="20"><use href="./images/svg-sprite-den.svg#arrow-right-icon"></use></svg></button>';
-  
-  const htmlUserLoginModal =
-    '<div class="menu-user-bar"><div class="menu-user-bar-foto"><svg class="menu-user-bar-icon" width="19" height="19"><use href="./images/svg-sprite-den.svg#user-default-icon"></use></svg></div><p class="menu-user-bar-name">Stephen</p></div><nav class="mob-menu-nav"><ul class="mob-menu-list-nav"><li class="mob-menu-item-nav"><a class="mob-menu-link-nav current" href="./index.html">HOME</a></li><li class="mob-menu-item-nav"><a class="mob-menu-link-nav" href="./shopping-list.html">SHOPPING LIST<svg class="header-link-icon" width="20" height="20"><use href="./images/svg-sprite-den.svg#bag"></use></svg></a></li></ul></nav><button class="menu-btn-exit" type="button">Log out<svg class="menu-btn-start-icon" width="20" height="20"><use href="./images/svg-sprite-den.svg#arrow-right-icon"></use></svg></button>';
-  
-  export function renderUserLogin(displayName) {
+const htmlUserNotLoginModal =
+  '<button data-modal-auth-open class="menu-btn-start" type="button">Sign up<svg class="menu-btn-start-icon" width="20" height="20"><use href="./images/svg-sprite-den.svg#arrow-right-icon"></use></svg></button>';
+
+const htmlUserLoginModal =
+  '<div class="menu-user-bar"><div class="menu-user-bar-foto"><svg class="menu-user-bar-icon" width="19" height="19"><use href="./images/svg-sprite-den.svg#user-default-icon"></use></svg></div><p class="menu-user-bar-name">Stephen</p></div><nav class="mob-menu-nav"><ul class="mob-menu-list-nav"><li class="mob-menu-item-nav"><a class="mob-menu-link-nav current" href="./index.html">HOME</a></li><li class="mob-menu-item-nav"><a class="mob-menu-link-nav" href="./shopping-list.html">SHOPPING LIST<svg class="header-link-icon" width="20" height="20"><use href="./images/svg-sprite-den.svg#bag"></use></svg></a></li></ul></nav><button class="menu-btn-exit" type="button">Log out<svg class="menu-btn-start-icon" width="20" height="20"><use href="./images/svg-sprite-den.svg#arrow-right-icon"></use></svg></button>';
+
+export function renderUserLogin(displayName) {
   menuStart.innerHTML = htmlUserLoginModal;
   const btnLogout = document.querySelector('.menu-btn-exit');
   btnLogout.addEventListener('click', onBtnLogout);
@@ -30,14 +30,16 @@ function openMenu() {
   burgerButton.classList.add('open');
   closeButton.classList.add('open');
   menuStart.classList.remove('is-hidden');
+  menuStart.classList.add('is-active-h');
   bodyElement.classList.add('no-scroll');
-  userVerification()
+  userVerification();
 }
 
 export function closeMenu() {
   burgerButton.classList.remove('open');
   closeButton.classList.remove('open');
-  menuStart.classList.add('is-hidden');
+  menuStart.classList.remove('is-active-h');
+  // menuStart.classList.add('is-hidden');
   bodyElement.classList.remove('no-scroll');
 }
 
@@ -62,11 +64,11 @@ function setUserName(displayName) {
 }
 
 function setUserNameTabDesc(displayName) {
-  const userNameTabletDesktop = document.querySelector('.user-name-tablet-desktop');
-  userNameTabletDesktop.textContent = displayName
-  
+  const userNameTabletDesktop = document.querySelector(
+    '.user-name-tablet-desktop'
+  );
+  userNameTabletDesktop.textContent = displayName;
 }
-
 
 burgerButton.addEventListener('click', openMenu);
 closeButton.addEventListener('click', closeMenu);
@@ -74,7 +76,6 @@ closeButton.addEventListener('click', closeMenu);
 window.addEventListener('resize', handleResize);
 
 // =========== Виклик меню "Виходу з аккаунта" ====================
-
 
 // headerBtnUser.addEventListener('click', () => {
 //   headerBtnUserMenu.classList.toggle('is-hidden');
@@ -101,9 +102,9 @@ window.addEventListener('resize', handleResize);
 
 // const headerNav = document.querySelector('.header-list-nav');
 // const menuBtnStart = document.querySelector('.menu-btn-start-tab');
-const headerButtonsEl = document.querySelector('.header-buttons')
-const headerBtnUserEl = document.querySelector('.header-btn-user-wrap')
-const headerNavLinkEl = document.querySelector('.header-nav')
+const headerButtonsEl = document.querySelector('.header-buttons');
+const headerBtnUserEl = document.querySelector('.header-btn-user-wrap');
+const headerNavLinkEl = document.querySelector('.header-nav');
 const headerBtnUserMenu = document.querySelector('.header-btn-user-menu');
 
 // export function removeHeaderHidden() {
@@ -116,14 +117,13 @@ const headerBtnUserMenu = document.querySelector('.header-btn-user-menu');
 // ========== not registration user ====================
 function renderBtnSignupTabDesc() {
   const markupSignupTabDesc = `<button data-modal-auth-open class="menu-btn-start-tab" type="button">Sign up<svg class="menu-btn-start-icon" width="20" height="20"><use href="./images/symbol-defs.svg#icon-arrow-right"></use></svg></button>`;
-    headerButtonsEl.insertAdjacentHTML('beforeend', markupSignupTabDesc);
-
+  headerButtonsEl.insertAdjacentHTML('beforeend', markupSignupTabDesc);
 }
 
 // ========== yes registration user ====================
 function renderNavLinkPageTabDesc() {
   const markupNavLinkPageTabDesc = `<ul class="header-list-nav"><li><a class="header-link-nav current" href="./index.html">HOME</a></li><li><a class="header-link-nav" href="./shopping-list.html">SHOPPING LIST<svg class="header-link-icon" width="20" height="20"><use href="./images/svg-sprite-den.svg#bag"></use></svg></a></li></ul>`;
-  headerNavLinkEl.insertAdjacentHTML('beforeend', markupNavLinkPageTabDesc)
+  headerNavLinkEl.insertAdjacentHTML('beforeend', markupNavLinkPageTabDesc);
 }
 
 function renderBtnUserProfTabDesc() {
@@ -132,7 +132,6 @@ function renderBtnUserProfTabDesc() {
   const headerBtnUser = document.querySelector('.header-btn-user');
   headerBtnUser.addEventListener('click', onOpenMenuUser);
   onOpenMenuUser();
-
 }
 
 function renderBtnLogoutTabDesc() {
@@ -144,14 +143,14 @@ function onOpenMenuUser() {
   headerBtnUserMenu.classList.toggle('is-hidden');
 }
 
-// ========== render after checking the user in firebase ================  
+// ========== render after checking the user in firebase ================
 export function renderHeaderTabDescLogin(displayName) {
   renderNavLinkPageTabDesc();
   renderBtnLogoutTabDesc();
   renderBtnUserProfTabDesc();
   setUserNameTabDesc(displayName);
   console.log('renderHeaderTabDescLogin');
-};
+}
 
 export function renderHeaderTabDescLogout() {
   renderBtnSignupTabDesc();
