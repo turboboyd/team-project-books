d2f22b1f5d07b0e44485d2b6aa24374cb45a1368
turboboyd <113061164+turboboyd@users.noreply.github.com>
2023-07-05 21:33:17 +0000

    Deploying to gh-pages from @ turboboyd/team-project-books@0aee9b470a68fdabfe48a0f3bd17996ed154d4b8 🚀

--- assets/shopping-list-42d0142b.js
+++ assets/shopping-list-42d0142b.js
@@ -1,4 +1,4 @@
-import{s as m,a as x,b as N,c as B,d as A,e as z,f as M}from"./book-shop@2x-397bd6a9.js";function r(t){try{return JSON.parse(localStorage.getItem(t))}catch(n){console.log(n)}}function W(t){return t.map((e,i)=>`<li class="shopping-item">
+import{s as m,a as x,b as N,c as B,d as A,e as z,f as M}from"./book-shop@2x-929b0335.js";function r(t){try{return JSON.parse(localStorage.getItem(t))}catch(n){console.log(n)}}function W(t){return t.map((e,i)=>`<li class="shopping-item">
     <img
       class="shopping-book-cover"
       src="${e.book_image}"
