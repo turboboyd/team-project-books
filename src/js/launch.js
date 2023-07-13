e16b41c8f7b9eae0f21c6b0d455df73d1e713901
StanislavPavlenko <voltfrag@gmail.com>
2023-07-13 17:53:43 +0300

    sesion-storage

--- src/js/launch.js
+++ src/js/launch.js
@@ -3,29 +3,84 @@ import { showLoader, hideLoader } from './loader';
 import Notiflix from 'notiflix';
 import renderWrapCategories from './bestsellers';
 import renderCategories from './filter';
+import * as Cache from '../js/support/sesion-storage-api';
 
 const bookApi = new BookAPI();
 
 generatePage();
 
-function generatePage() {
-  showLoader();
-  Promise.all([
-    bookApi.getBooksCategoriesList().then(data => {
+async function generatePage() {
+  try {
+    showLoader();
+    if (!Cache.getData('categories')) {
+      const data = await bookApi.getBooksCategoriesList();
+      Cache.setData('categories', data);
       renderCategories(data);
-    }),
-    bookApi.getTopBooks().then(data => {
-      renderWrapCategories(data);
-    }),
-  ])
-    .then(() => {
       hideLoader();
-    })
-    .catch(error => {
-      console.error('Error:', error);
+    } else {
+      const categories = Cache.getData('categories');
+      renderCategories(categories);
+    }
+    if (!Cache.getData('Top-Books')) { //if cache is not empty
+      const topBooksData = await bookApi.getTopBooks();
+      Cache.setData('Top-Books', topBooksData); // write to cache
+      renderWrapCategories(topBooksData);
       hideLoader();
-      Notiflix.Notify.failure(
-        'Oops! Something went wrong. Please try again later.'
-      );
-    });
+    } else {// Read Cached data
+      const topBooks = Cache.getData('Top-Books');
+      renderWrapCategories(topBooks);
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    hideLoader();
+    Notiflix.Notify.failure(
+      'Oops! Something went wrong. Please try again later.'
+    );
+  } finally {
+       hideLoader();
+  }
 }
+
+// function generatePage() {
+//   if (!Cache.getData('categories')) {
+//     bookApi
+//       .getBooksCategoriesList()
+//       .then(data => {
+//         Cache.setData('categories', data);
+//         renderCategories(data);
+//       })
+//       .then(() => {
+//         hideLoader();
+//       })
+//       .catch(error => {
+//
+//       });
+//   } else  {
+//
+//   }
+
+//   if (!Cache.getData('Top-Books')) {
+//     showLoader();
+
+//     bookApi
+//       .getTopBooks()
+//       .then(data => {
+//         Cache.setData('Top-Books', data);
+//         renderWrapCategories(data);
+//       })
+
+//       .then(() => {
+//         hideLoader();
+//       })
+//       .catch(error => {
+//         console.error('Error:', error);
+//         hideLoader();
+//         Notiflix.Notify.failure(
+//           'Oops! Something went wrong. Please try again later.'
+//         );
+//       });
+//   } else if (Cache.getData('Top-Books')) {
+//     const topBooks = Cache.getData('Top-Books');
+//     renderWrapCategories(topBooks);
+//   }
+// }
